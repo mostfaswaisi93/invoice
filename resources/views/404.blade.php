0ddf2d94a29f4352fbf4d88b575deb3b944b292a
Mustafa Al-Swaisi <mostfaswaisi93@gmail.com>
2020-11-28 16:50:20 +0200

    Added Files

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.master2')
+
 @section('css')
 <!--- Internal Fontawesome css-->
 <link href="{{URL::asset('assets/plugins/fontawesome-free/css/all.min.css')}}" rel="stylesheet">
@@ -11,14 +12,17 @@
 <!---Internal Falg-icons css-->
 <link href="{{URL::asset('assets/plugins/flag-icon-css/css/flag-icon.min.css')}}" rel="stylesheet">
 @endsection
+
 @section('content')
-		<!-- Main-error-wrapper -->
-		<div class="main-error-wrapper  page page-h ">
-			<img src="{{URL::asset('assets/img/media/404.png')}}" class="error-page" alt="error">
-			<h2>Oopps. The page you were looking for doesn't exist.</h2>
-			<h6>You may have mistyped the address or the page may have moved.</h6><a class="btn btn-outline-danger" href="{{ url('/' . $page='index') }}">Back to Home</a>
-		</div>
-		<!-- /Main-error-wrapper -->
+<!-- Main-error-wrapper -->
+<div class="main-error-wrapper  page page-h ">
+	<img src="{{URL::asset('assets/img/media/404.png')}}" class="error-page" alt="error">
+	<h2>Oopps. The page you were looking for doesn't exist.</h2>
+	<h6>You may have mistyped the address or the page may have moved.</h6><a class="btn btn-outline-danger"
+		href="{{ url('/' . $page='index') }}">Back to Home</a>
+</div>
+<!-- /Main-error-wrapper -->
 @endsection
+
 @section('js')
 @endsection
