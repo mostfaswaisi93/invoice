f881f136f5d209fd0a2e88049e3a1e4cd51d8eeb
Mustafa Al-Swaisi <mostfaswaisi93@gmail.com>
2020-11-28 12:31:19 +0200

    Auth

--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Auth::routes();
+Auth::routes(['register' => false]);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
 Route::get('/{page}', [AdminController::class, 'index']);
